6291300c5b0b3b324a5a468a77fdea524b466bd8
Przemysław Jurkiewicz <przemko.jurkiewicz@gmail.com>
2019-04-10 13:30:14 +0200

    Add reducer

---
+++ comments.js
@@ -0,0 +1,44 @@
+import {
+  ADD_COMMENT,
+  REMOVE_COMMENT,
+  THUMB_DOWN_COMMENT,
+  THUMB_UP_COMMENT
+} from './actions';
+
+function comments(state = [], action) {
+  switch (action.type) {
+    case ADD_COMMENT:
+      return [
+        {
+          id: action.id,
+          text: action.text,
+          votes: 0
+        },
+        ...state
+      ];
+
+    case REMOVE_COMMENT:
+      return state.filter(comments => comments.id !== action.id);
+
+    case THUMB_UP_COMMENT:
+      return state.map(comment => {
+        if (comment.id === action.id) {
+          return { ...comment, votes: comment.votes + 1 };
+        }
+        return comment;
+      });
+
+    case THUMB_DOWN_COMMENT:
+      return state.map(comment => {
+        if (comment.id === action.id) {
+          return { ...comment, votes: comment.votes - 1 };
+        }
+        return comment;
+      });
+
+    default:
+      return state;
+  }
+}
+
+export default comments;
