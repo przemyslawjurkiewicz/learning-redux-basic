9816875cb8176a6af1742a3b1442d16c241ab0ae
Przemysław Jurkiewicz <przemko.jurkiewicz@gmail.com>
2019-04-10 12:09:30 +0200

    Add actions.js

---
+++ actions.js
@@ -0,0 +1,44 @@
+const ADD_COMMENT = 'ADD_COMMENT';
+const REMOVE_COMMENT = 'REMOVE_COMMENT';
+const EDIT_COMMENT = 'EDIT_COMMENT';
+const THUMB_UP_COMMENT = 'THUMB_UP_COMMENT';
+const THUMB_DOWN_COMMENT = 'THUMB_DOWN_COMMENT';
+
+function addComment(text) {
+    return {
+        type: ADD_COMMENT,
+        text,
+        id: uuid.v4()
+    }
+}
+
+function editComment(id) {
+    return {
+        type: EDIT_COMMENT,
+        id,
+        text
+    }
+}
+
+function removeComment(id) {
+    return {
+        type: REMOVE_COMMENT,
+        id
+    }
+}
+
+function thumbUpComment(id) {
+    return {
+        type: THUMB_UP_COMMENT,
+        id,
+        votes
+    }
+}
+
+function thumbDownComment(id) {
+    return {
+        type: THUMB_DOWN_COMMENT,
+        id,
+        votes
+    }
+}
